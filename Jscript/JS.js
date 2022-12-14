b42f78834bd9572e045b999d09a410155a3bd9db
spydeey <97867252+spydeey@users.noreply.github.com>
2022-12-14 21:14:50 +0530

    Update JS.js

--- Jscript/JS.js
+++ Jscript/JS.js
@@ -1,1 +1,8 @@
+var hamburger = document.querySelector(".hamburger-menu");
+var navlist = document.querySelector(".nav-list");
+var links = document.querySelector(".nav-list li");
 
+hamburger.addEventListener("click", function(){
+    this.classList.toggle(click);
+    navlist.classList.toggle("open");
+})
